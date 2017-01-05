36155e2ec3a1017f49fbc3611ca4bfe8a8cf35f0
Bruno STEVANT <bruno.stevant@telecom-bretagne.eu>
2017-01-05 16:47:45 +0100

    Add std deviation % to analysis

--- ansible/analyse_iperf.py
+++ ansible/analyse_iperf.py
@@ -34,5 +34,5 @@ for dirname in os.listdir('logs'):
         text_file.close()
 for pair in pairs:
     avg = np.average(results[pair])
-    std = np.std(results[pair])
-    print pair + ": " + str(avg) + " dev: " + str(std) + " (" + str(len(results[pair])) + " results)"
+    std = int(np.std(results[pair]) * 100 / avg)
+    print pair + ": " + str(avg) + " dev: " + str(std) + "% (" + str(len(results[pair])) + " results)"
