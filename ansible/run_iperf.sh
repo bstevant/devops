c3dabe67ed15657543af4dbbd1cea908dd5ea6f5
Bruno STEVANT <bruno.stevant@gmail.com>
2017-01-05 14:17:46 +0100

    Add script to run all iperf tests

---
+++ ansible/run_iperf.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+BASEDIR=/vagrant/ansible
+LOGDIR=`date +%Y-%m-%d-%H:%M`
+mkdir $BASEDIR/logs/$LOGDIR
+rm -f $BASEDIR/logs/current
+ln -s $LOGDIR $BASEDIR/logs/current
+ansible-playbook $BASEDIR/iperf.yml -i $BASEDIR/hosts/iperf
+ansible-playbook $BASEDIR/iperf-stop.yml -i $BASEDIR/hosts/iperf
+ansible-playbook $BASEDIR/iperf.yml -i $BASEDIR/hosts/iperf2
+ansible-playbook $BASEDIR/iperf-stop.yml -i $BASEDIR/hosts/iperf2
+ansible-playbook $BASEDIR/iperf.yml -i $BASEDIR/hosts/iperf3
+ansible-playbook $BASEDIR/iperf-stop.yml -i $BASEDIR/hosts/iperf3
+ansible-playbook $BASEDIR/iperf.yml -i $BASEDIR/hosts/iperf4
+ansible-playbook $BASEDIR/iperf-stop.yml -i $BASEDIR/hosts/iperf4
