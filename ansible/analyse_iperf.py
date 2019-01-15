b852511ec25a86c8ee6c0c6ebc9a57cd5c7b3c24
Bruno STEVANT <bruno.stevant@gmail.com>
2019-01-15 11:41:39 +0100

    Add filtered scores

--- ansible/analyse_iperf.py
+++ ansible/analyse_iperf.py
@@ -1,37 +1,41 @@
 import os
 import numpy as np
 
-pairs = [ "fog8-fog11", 
-          "fog8-fog10",
-          "fog8-fog9",
-          "fog9-fog11", 
-          "fog9-fog10",
-          "fog9-fog8",
-          "fog10-fog11", 
-          "fog10-fog8",
-          "fog10-fog9",
-          "fog11-fog8", 
-          "fog11-fog9",
-          "fog11-fog10"
+pairs = [ 
+          "fog9-fog10", 
+          "fog9-fog11",
+          "fog9-fog12",
+          "fog10-fog9a", 
+          "fog10-fog11",
+          "fog10-fog12",
+          "fog11-fog9a",
+          "fog11-fog10", 
+          "fog11-fog12",
+          "fog12-fog9a",
+          "fog12-fog10", 
+          "fog12-fog11"
       ]
 results = {}
 
 for dirname in os.listdir('logs'):
     for pair in pairs:
-        text_file = open("logs/"+ dirname + "/iperf-" + pair + ".ipv6.enstb.fr", "r")
-        lines = text_file.readlines()
-        l = lines[0]
-        a = eval(l)
-        s = a[16]
-        sp = s.split()
-        res = float(sp[6])
-        unit = sp[7]
-        if unit == 'Mbits/sec':
-            res *= 1000
-        if pair not in results.keys():
-            results[pair] = []
-        results[pair].append(res)
-        text_file.close()
+	try:
+        	text_file = open("logs/"+ dirname + "/iperf-" + pair + ".ipv6.enstb.fr", "r")
+        	lines = text_file.readlines()
+        	l = lines[0]
+        	a = eval(l)
+        	s = a[16]
+        	sp = s.split()
+        	res = float(sp[6])
+        	unit = sp[7]
+        	if unit == 'Mbits/sec':
+            		res *= 1000
+        	if pair not in results.keys():
+            		results[pair] = []
+        	results[pair].append(res)
+        	text_file.close()
+	except:
+		continue
 for pair in pairs:
     avg = np.average(results[pair])
     std = int(np.std(results[pair]) * 100 / avg)
