c9d1b641b0870c02629cbbb5208a6bbc587d0619
Bruno STEVANT <bruno.stevant@gmail.com>
2019-03-08 23:51:14 +0100

    Fix

--- filter_scenario.py
+++ filter_scenario.py
@@ -1,17 +1,27 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('filepath', type=str, help='file to parse')
+parser.add_argument('filepath', type=str, help='dirname')
+parser.add_argument('node', type=str, help='node')
 parser.add_argument('gap', type=int, help='Gap btw scenario (in s)')
 args = parser.parse_args()
 
 nb_scenario = 0
-with open(args.filepath, 'r') as resfile:
-    last_ts = 0
-    for l in resfile.readlines():
-        s = l.split(' ')
-        ts = int(s[0])
-        if (ts - last_ts) > args.gap:
-            nb_scenario+=1
+filenames = ['iperf-fog3n1.ipv6.enstb.fr-%s',
+             'iperf-%s-fog3n1.ipv6.enstb.fr' ]
+scenarios = []
+
+for f in filenames:
+    path =  args.filepath + (f % args.node)
+    with open(args.filepath, 'r') as resfile:
+        last_ts = 0
+        for l in resfile.readlines():
+            s = l.split(' ')
+            ts = int(s[0])
+            if (ts - last_ts) > args.gap:
+                nb_scenario+=1
+                if last_ts != 0:
+                    scenarios.append([start_ts, last-ts])
+                start_ts = ts
             last_ts = ts
-print("%s: %d" % (args.filepath, nb_scenario))
+    print("%s: %d" % (path, nb_scenario))
--- filter_scenario.sh
+++ filter_scenario.sh
@@ -2,7 +2,5 @@ nodes="fog8 fog9 fog10 fog11 fog12 fog13 fog14 g6fog"
 gap=3600
 
 for i in $nodes; do
-  python filter_scenario.py ansible/logs/iperf/iperf-fog3n1.ipv6.enstb.fr-$i $gap
-  python filter_scenario.py ansible/logs/iperf/iperf-$i-fog3n1.ipv6.enstb.fr $gap
-  python filter_scenario.py ansible/logs/ping/ping-$i-fog3n1.ipv6.enstb.fr $gap
+  python filter_scenario.py ansible/logs/iperf/ $i $gap
 done
