99200f6b582b4f8f833f9a4de150f7746f592f0d
Bruno STEVANT <bruno.stevant@gmail.com>
2019-03-11 16:34:05 +0100

    Fix

--- filter_iperf2.sh
+++ filter_iperf2.sh
@@ -8,9 +8,9 @@ scn14="fog8 fog10 fog12 fog13 fog14 g6fog"
 
 for i in $scn1; do
   echo "time bw" > filtered_scores/scn1_downbw_$i.txt
-  python filter_iperf.py ansible/logs/iperf/iperf-fog3n1.ipv6.enstb.fr-$i 1547047314 1547064073 >> filtered_scores/scn1_downbw_$i.txt
+  python filter_iperf.py ansible/logs/iperf/iperf-fog3n1.ipv6.enstb.fr-$i 1547856157 1548411965 >> filtered_scores/scn1_downbw_$i.txt
   echo "time bw" > filtered_scores/scn1_upbw_$i.txt
-  python filter_iperf.py ansible/logs/iperf/iperf-$i-fog3n1.ipv6.enstb.fr 1547047314 1547064073 >> filtered_scores/scn1_upbw_$i.txt
+  python filter_iperf.py ansible/logs/iperf/iperf-$i-fog3n1.ipv6.enstb.fr 1547856157 1548411965 >> filtered_scores/scn1_upbw_$i.txt
   echo "time ms" > filtered_scores/scn1_ping_$i.txt
-  python filter_ping.py ansible/logs/ping/ping-$i-fog3n1.ipv6.enstb.fr 1547047314 1547064073 >> filtered_scores/scn1_ping_$i.txt
+  python filter_ping.py ansible/logs/ping/ping-$i-fog3n1.ipv6.enstb.fr 1547856157 1548411965 >> filtered_scores/scn1_ping_$i.txt
 done
